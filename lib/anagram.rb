c8ce3ca09e5dd51b71f8794e779215f412d4d813
Uriah Harston <uharston@gmail.com>
2020-05-16 20:48:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  #setter assigns an instance a value
+  #getter is what is called on an instance to reveal that value
+  #initialize can assigns value to an instance on initialization
+  #initialize also excecute given commands to every new instance
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  #Check if self.word has a matching anagrams
+  #How do we compare self.word to an array of strings - iterate
+  #How to compare a string to each string in an array
+  #%w(desserts tacobar creperie kayak) is a shortcut for 
+  #writing arrays with spaces as the delimiter instead of commas
+  def match(anagram_array)
+    anagram_array.each do |compare|
+      
+      self.word
+  end
+
+end
+
+listen = Anagram.new("listen")
